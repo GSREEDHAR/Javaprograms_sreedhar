331e147f46548fc3595ba51a8a28728e903e48fd
Sreedhar <mailtogsreedhar@yahoo.in>
2021-05-04 00:04:45 +0530

    Java programs

---
+++ Sampleprograms/src/main/java/Javaprograms/Strings.java
@@ -0,0 +1,14 @@
+package Javaprograms;
+
+public class Strings {
+
+	public static void main(String[] args)
+	{
+		String ss ="Sreedhar";
+		System.out.println("String:"+ss);
+	
+	int length= ss.length();
+	System.out.println("Length:" +length);
+	}
+	
+}
